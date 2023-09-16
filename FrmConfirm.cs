ccaa1b76490c60e33da31325db0fbc6b066ab30d
Sem Jafet Cabarles <98858808+Sejimin-9@users.noreply.github.com>
2023-09-16 22:38:23 +0800

    Add files via upload

---
+++ FrmConfirm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Account_Registration
+{
+    public partial class FrmConfirm : Form
+    {
+        public static FrmConfirm instance;
+        public static DelegateText DelProgram, DelLastName, DelFirstName,  DelMiddleName, DelAddress;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+            
+        }
+
+        private DelegateNumber DelNumAge, DelNumContactNo, DelStudNo;
+
+        public FrmConfirm()
+        {
+            InitializeComponent();
+            instance = this;
+            DelStudNo = new DelegateNumber(StudentInfoClass.GetStudentNo);
+            DelProgram = new DelegateText(StudentInfoClass.GetProgram);
+            DelLastName = new DelegateText(StudentInfoClass.GetLastName);
+            DelFirstName = new DelegateText (StudentInfoClass.GetFirstName);
+            DelMiddleName = new DelegateText(StudentInfoClass.GetMiddleName);
+            DelNumAge = new DelegateNumber(StudentInfoClass.GetAge);
+            DelNumContactNo = new DelegateNumber(StudentInfoClass.GetContactNo);
+            DelAddress = new DelegateText(StudentInfoClass.GetAddress);
+
+
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmConfirm_Load(object sender, EventArgs e)
+        {
+            label9.Text = DelStudNo(StudentInfoClass.StudentNo).ToString();
+            label10.Text = DelProgram(StudentInfoClass.Program).ToString();
+            label11.Text = DelLastName(StudentInfoClass.LastName).ToString();
+            label12.Text = DelFirstName(StudentInfoClass.FirstName).ToString(); 
+            label13.Text = DelMiddleName(StudentInfoClass.MiddleName).ToString();   
+            label14.Text = DelNumAge(StudentInfoClass.Age).ToString();
+            label15.Text = DelNumContactNo(StudentInfoClass.ContactNo).ToString();
+            label16.Text = DelAddress(StudentInfoClass.Address).ToString();
+
+
+
+
+
+        }
+    }
+}
---
+++ FrmRegistration.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Account_Registration
+{
+    public partial class FrmRegistration : Form
+    {
+        public static string value;
+        public static FrmRegistration inst;
+        StudentInfoClass sem = new StudentInfoClass();
+        public FrmRegistration()
+        {
+            inst = this;
+            InitializeComponent();
+        }
+        private DelegateText dValue;
+        private DelegateNumber dNum;
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StudentInfoClass.StudentNo = Int64.Parse(textBox1.Text);
+            StudentInfoClass.Program = BSBA.Text.ToString();
+            StudentInfoClass.LastName = textBox2.Text.ToString();
+            StudentInfoClass.FirstName = textBox5.Text.ToString();
+            StudentInfoClass.MiddleName = textBox7.Text.ToString();
+            StudentInfoClass.Age = Int64.Parse(textBox3.Text);
+            StudentInfoClass.ContactNo = Int64.Parse(textBox6.Text);
+            StudentInfoClass.Address = textBox4.Text.ToString();
+     
+            FrmConfirm fcm = new FrmConfirm();
+            fcm.ShowDialog();
+        }
+    }
+}
